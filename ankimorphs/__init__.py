2dd53093293e215e7c0850a0b0138c33c11f8683
BuildTools <mariothrowsfieball@gmail.com>
2023-11-10 06:14:48 +0100

    removed old commits

--- ankimorphs/__init__.py
+++ ankimorphs/__init__.py
@@ -11,7 +11,6 @@
 # Incorrect:
 # from ankimorphs import browser_utils
 ################################################################
-import os
 from functools import partial
 
 from aqt import gui_hooks, mw
@@ -70,7 +69,6 @@ def main() -> None:
     gui_hooks.sync_will_start.append(recalc_on_sync)
 
     gui_hooks.profile_will_close.append(clear_seen_morphs)
-    gui_hooks.webview_will_show_context_menu.append(add_name)
 
 
 def init_toolbar_items(links: list[str], toolbar: Toolbar) -> None:
@@ -393,15 +391,6 @@ def create_already_known_tagger_action(am_config: AnkiMorphsConfig) -> QAction:
     return action
 
 
-def add_name(web_view, menu):
-    selected_text = web_view.selectedText()
-    if selected_text == "":
-        return
-    action = QAction("Mark as name", menu)
-    action.triggered.connect(lambda: add_name_to_file(selected_text))
-    menu.addAction(action)
-
-
 def create_test_action() -> QAction:
     keys = QKeySequence("Ctrl+T")
     action = QAction("&Test", mw)
@@ -438,14 +427,6 @@ def test_function() -> None:
     print(f"result?: {result}")
 
     am_db.con.close()
-
-
-def add_name_to_file(name: str):
-    profile_path: str = mw.pm.profileFolder()
-    path = os.path.join(profile_path, "names.txt")
-    file = open(path, "a")
-    file.write("\n" + name)
-    file.close()
 
 
 main()
