870ae33143646329f204c4955b093791a7557d0c
mortii <mortteenn@gmail.com>
2023-11-18 16:02:50 +0100

    bundled addon now contains version in the filename

--- bundle_addon.sh
+++ bundle_addon.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 cd ankimorphs || { echo "cd failed"; exit 1; }
+version="v$(grep -Po '(?<=version: )[^\"]*' settings_dialog.py)"
+version=$(echo "$version" | tr . -)  # replace . with - for filename to work
 find . -regex '^.*\(__pycache__\|\.py[co]\)$' -delete
 rm meta.json
-zip -r ../ankimorphs.ankiaddon ./*
+zip -r ../ankimorphs-"$version".ankiaddon ./*
 cd ..
