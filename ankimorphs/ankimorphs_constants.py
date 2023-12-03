25c7ad20ed15a60ed81e871a71918a71ed688247
mortii <mortteenn@gmail.com>
2023-12-03 13:15:43 +0100

    bundle_addon.sh now automatically sets DEV_MODE to false

--- ankimorphs/ankimorphs_constants.py
+++ ankimorphs/ankimorphs_constants.py
@@ -4,7 +4,7 @@ If A constant is only used in one file then it should be declared in
 that file and not here.
 """
 
-DEV_MODE: bool = True
+DEV_MODE: bool = False
 SETTINGS_DIALOG_NAME: str = "am_settings_dialog"
 TAG_SELECTOR_DIALOG_NAME: str = "am_tag_selector_dialog"
 FREQUENCY_FILE_GENERATOR_DIALOG_NAME: str = "am_frequency_file_dialog"
--- bundle_addon.sh
+++ bundle_addon.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
 cd ankimorphs || { echo "cd failed"; exit 1; }
+
+# If DEV_MODE = True, set it to False
+found_dev_mode=$(grep -cE "DEV_MODE: bool = (True|False)" ankimorphs_constants.py)
+[ "$found_dev_mode" -eq 1 ] || { echo "grep result was not 1!"; exit 1; }
+sed -i 's/DEV_MODE: bool = True/DEV_MODE: bool = False/g' ankimorphs_constants.py
+
+# Find and store the AnkiMorphs version number. Will be used in .addon file name
 version="v$(grep -Po '(?<=version: )[^\"]*' settings_dialog.py)"
 version=$(echo "$version" | tr . -)  # replace . with - for filename to work
+
+# all pycache files have to be deleted before we can zip the .addon file
 find . -regex '^.*\(__pycache__\|\.py[co]\)$' -delete
+
+# meta.json is the local user's customized version of config.json, don't bundle this.
 rm meta.json
+
 zip -r ../ankimorphs-"$version".ankiaddon ./*
+
 cd ..
