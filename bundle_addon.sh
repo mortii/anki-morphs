9a91c5d90db318592f85a9d1914c1018fcaef9fd
mortii <mortteenn@gmail.com>
2023-12-14 12:10:07 +0100

    added check for explicit imports of 'ankimorphs' module (#98)

--- bundle_addon.sh
+++ bundle_addon.sh
@@ -1,18 +1,33 @@
 #!/bin/sh
 
-cd ankimorphs || { echo "cd failed"; exit 1; }
 
-# If DEV_MODE = True, set it to False
+abort_bundle() {
+  echo "$1"  # input parameter
+  echo "aborting bundle"
+  cd ..
+  kill -INT $$  # the equivalent of ctrl + c
+}
+
+cd ankimorphs || { echo "cd failed"; return 1; }
+
+# all pycache files have to be deleted before we can zip the .addon file
+find . -regex '^.*\(__pycache__\|\.py[co]\)$' -delete
+
+# explicitly importing 'ankimorphs' breaks the addon
+if grep -rqE --exclude-dir=deps "^(from|import) ankimorphs"; then
+  abort_bundle "found explicit imports"
+fi
+
+# set DEV_MODE = False
 found_dev_mode=$(grep -cE "DEV_MODE: bool = (True|False)" ankimorphs_globals.py)
-[ "$found_dev_mode" -eq 1 ] || { echo "grep result was not 1!"; exit 1; }
+if [ "$found_dev_mode" != 1 ]; then
+  abort_bundle "the number of DEV_MODE instances is not 1"
+fi
 sed -i 's/DEV_MODE: bool = True/DEV_MODE: bool = False/g' ankimorphs_globals.py
 
 # Find and store the AnkiMorphs version number. Will be used in .addon file name
 version="v$(grep -Po '(?<=version: )[^\"]*' settings_dialog.py)"
 version=$(echo "$version" | tr . -)  # replace . with - for filename to work
-
-# all pycache files have to be deleted before we can zip the .addon file
-find . -regex '^.*\(__pycache__\|\.py[co]\)$' -delete
 
 # meta.json is the local user's customized version of config.json, don't bundle this.
 rm meta.json
