a324382e0a04e5ee20271eb0d54f8fc4d3b28544
mortii <mortteenn@gmail.com>
2023-12-15 21:23:52 +0100

    bumped ankimorphs version

--- ankimorphs/settings_dialog.py
+++ ankimorphs/settings_dialog.py
@@ -92,7 +92,7 @@ class SettingsDialog(QDialog):  # pylint:disable=too-many-instance-attributes
         )
 
         # Semantic Versioning https://semver.org/
-        self.ui.ankimorphs_version_label.setText("AnkiMorphs version: 0.9.0-alpha")
+        self.ui.ankimorphs_version_label.setText("AnkiMorphs version: 0.10.0-alpha")
 
         self.show()
 
