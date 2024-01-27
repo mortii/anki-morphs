36b4458a19d97033e5c248e653d13e9ef38a2782
mortii <mortteenn@gmail.com>
2024-01-27 13:38:10 +0100

    tags should now be mutually exclusive (#139)

--- ankimorphs/recalc.py
+++ ankimorphs/recalc.py
@@ -783,31 +783,52 @@ def _update_tags_and_queue(
     unknowns: int,
     has_learning_morphs: bool,
 ) -> None:
-    suspended = CardQueue(-1)
+    # There are 3 different tags that we want recalc to update:
+    # - am-ready
+    # - am-not-ready
+    # - am-known-automatically
+    #
+    # These tags should be mutually exclusive, and there are many
+    # complicated scenarios where a normal tag progression might
+    # not occur, so we have to make sure that we remove all the
+    # tags that shouldn't be there for each case, even if it seems
+    # redundant.
+    #
+    # Note: only new cards are handled in this function!
 
-    if unknowns == 0:
-        # if a card has any learning morphs, then we don't want to
-        # give it a 'known' tag because that would automatically
-        # give the morph a 'known'-status instead of 'learning'
-        if not has_learning_morphs:
-            # if a card was 'set known and skip' then it already has
-            # the 'manually known' tag, don't add 'automatically known'
-            if am_config.tag_known_manually not in note.tags:
-                if am_config.tag_known_automatically not in note.tags:
-                    note.tags.append(am_config.tag_known_automatically)
-        if am_config.tag_ready in note.tags:
-            note.tags.remove(am_config.tag_ready)
-        if am_config.recalc_suspend_known_new_cards:
-            if card.queue != suspended:
-                card.queue = suspended
+    suspended = CardQueue(-1)
+    mutually_exclusive_tags: list[str] = [
+        am_config.tag_ready,
+        am_config.tag_not_ready,
+        am_config.tag_known_automatically,
+    ]
+
+    if am_config.tag_known_manually in note.tags:
+        remove_exclusive_tags(note, mutually_exclusive_tags)
+    elif unknowns == 0:
+        if am_config.recalc_suspend_known_new_cards and card.queue != suspended:
+            card.queue = suspended
+        if am_config.tag_known_automatically not in note.tags:
+            remove_exclusive_tags(note, mutually_exclusive_tags)
+            # if a card has any learning morphs, then we don't want to
+            # give it a 'known' tag because that would automatically
+            # give the morphs a 'known'-status instead of 'learning'
+            if not has_learning_morphs:
+                note.tags.append(am_config.tag_known_automatically)
     elif unknowns == 1:
         if am_config.tag_ready not in note.tags:
+            remove_exclusive_tags(note, mutually_exclusive_tags)
             note.tags.append(am_config.tag_ready)
-        if am_config.tag_not_ready in note.tags:
-            note.tags.remove(am_config.tag_not_ready)
     else:
         if am_config.tag_not_ready not in note.tags:
+            remove_exclusive_tags(note, mutually_exclusive_tags)
             note.tags.append(am_config.tag_not_ready)
+
+
+def remove_exclusive_tags(note: Note, mutually_exclusive_tags: list[str]) -> None:
+    for tag in mutually_exclusive_tags:
+        if tag in note.tags:
+            note.tags.remove(tag)
 
 
 def _highlight_text(
